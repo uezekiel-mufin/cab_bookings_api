48b7ce73209a99d552ab6e5af4d53830c9dcdd89
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-06-12 13:38:38 +0300

    Make initial cab table

---
+++ db/schema.rb
@@ -0,0 +1,32 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_092613) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cabs", force: :cascade do |t|
+    t.string "manufacturer"
+    t.text "description"
+    t.string "image_url"
+    t.string "transmission"
+    t.string "model"
+    t.float "rental_price"
+    t.integer "seating_capacity"
+    t.string "body_type"
+    t.integer "discount"
+    t.string "engine_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
