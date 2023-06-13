bc3832d59c96a4cd2472db551f0240a602b1937a
Outkarm <jkpordje@gmail.com>
2023-06-13 10:39:16 +0000

    fix all linter errors

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,7 +2,7 @@ class MembersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    user = get_user_from_token
+    user = generate_user_from_token
     render json: {
       message: 'You are logged in',
       user:
@@ -11,7 +11,7 @@ class MembersController < ApplicationController
 
   private
 
-  def get_user_from_token
+  def generate_user_from_token
     jwt_payload = JWT.decode(request.headers['Authorization'].split[1],
                              Rails.application.credentials.devise[:jwt_secret_key]).first
     user_id = jwt_payload['sub']
