db6ad6dd9e3a853ea37c51956a1dd7d01f5cc944
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-06-13 12:44:05 +0300

    Add create action

--- app/controllers/api/v1/cabs_controller.rb
+++ app/controllers/api/v1/cabs_controller.rb
@@ -1,2 +1,22 @@
-class CabsController < ApplicationController
+class Api::V1::CabsController < ApplicationController
+  def index
+    render json: Cab.all
+  end
+
+  def create
+    
+    @car = Cab.new(car_params.merge(user_id: current_user_id))
+
+    if @cab.save
+      render json: @cab, status: :created
+    else
+      render json: @cab, status: :unprocessable_entity
+    end
+  end
+  
+  private
+
+  def cab_params
+    params.require(:cab).permit(:manufacturer, :description, :image_url, :transmission, :model, :rental_price, :seating_capacity, :body_type, :discount, :engine_type)
+  end
 end
