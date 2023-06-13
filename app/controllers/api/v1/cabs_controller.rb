7fdbdd456b2d2af2191ab84d5af67128486db4bb
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-06-13 12:48:10 +0300

    Fix linters

--- app/controllers/api/v1/cabs_controller.rb
+++ app/controllers/api/v1/cabs_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::CabsController < ApplicationController
   end
 
   def create
-    
     @car = Cab.new(car_params.merge(user_id: current_user_id))
 
     if @cab.save
@@ -13,10 +12,11 @@ class Api::V1::CabsController < ApplicationController
       render json: @cab, status: :unprocessable_entity
     end
   end
-  
+
   private
 
   def cab_params
-    params.require(:cab).permit(:manufacturer, :description, :image_url, :transmission, :model, :rental_price, :seating_capacity, :body_type, :discount, :engine_type)
+    params.require(:cab).permit(:manufacturer, :description, :image_url, :transmission, :model, :rental_price,
+                                :seating_capacity, :body_type, :discount, :engine_type)
   end
 end
