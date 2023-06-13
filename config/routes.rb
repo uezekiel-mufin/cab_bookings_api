76eb89ab03f0bcaf28dbabdc4e8f1bb46a4f201c
Outkarm <jkpordje@gmail.com>
2023-06-13 09:59:08 +0000

    configure route to hundle changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users,
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registerations'
-  }
+  
   get '/member-data', to: 'members#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      devise_for :users,
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'api/v1/users/registrations'
+  }
       resources :cabs, only: [:index, :show, :update, :destroy]
     end
   end
   # Defines the root path route ("/")
-  root "homepages#index"
+  root "home#index"
 end
