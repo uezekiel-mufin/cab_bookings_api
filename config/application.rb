034cbec42035a70caea471e4dffb730af779baec
Outkarm <jkpordje@gmail.com>
2023-06-13 09:57:20 +0000

    configure application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module CabBookingsApi
 
     config.session_store :cookie_store, key: '_interslice_session'
 
+    config.middleware.use ActionDispatch::Flash
+
     config.middleware.use ActionDispatch::Cookies
 
     config.middleware.use config.session_store, config.session_options
