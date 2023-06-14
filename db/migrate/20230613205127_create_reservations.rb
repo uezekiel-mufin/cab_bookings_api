58cfda0ad331c0d9cbc3624ecf8572384b45cdb3
Outkarm <jkpordje@gmail.com>
2023-06-14 15:01:47 +0000

    generate reservation migration

--- db/migrate/20230613205127_create_reservations.rb
+++ db/migrate/20230613205127_create_reservations.rb
@@ -2,7 +2,7 @@ class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :user_name, null: false
+      t.string :city, null: false
       t.references :cab, null: false, foreign_key: true
       t.datetime :reserve_date
 
