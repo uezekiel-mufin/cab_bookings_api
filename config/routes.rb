6c4de8651ad8df68146acaedab70e46f2d0084ec
Outkarm <jkpordje@gmail.com>
2023-06-14 10:04:41 +0000

    add routes to reservations

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-
   get '/member-data', to: 'members#show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   namespace :api do
     namespace :v1 do
       devise_for :users,
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'api/v1/users/registrations'
-  }
-      resources :cabs, only: [:index, :show, :update, :destroy]
+                 controllers: {
+                   sessions: 'users/sessions',
+                   registrations: 'api/v1/users/registrations'
+                 }
+      resources :cabs, only: [:index, :show, :update, :destroy] do
+        resources :reservations, only: [:index, :show, :update, :destroy]
+      end
     end
   end
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+
   root "home#index"
 end
