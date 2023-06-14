6fe39d5c5ffe876f37c6b0a01ab3deec966191ab
Outkarm <jkpordje@gmail.com>
2023-06-14 09:59:54 +0000

    update schema with reservations migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_203115) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_205127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cabs", force: :cascade do |t|
+    t.string "manufacturer"
+    t.text "description"
+    t.string "image_url"
+    t.string "transmission"
+    t.string "model"
+    t.float "rental_price"
+    t.integer "seating_capacity"
+    t.string "body_type"
+    t.integer "discount"
+    t.string "engine_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
@@ -20,6 +35,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_203115) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "user_name"
+    t.bigint "cab_id", null: false
+    t.datetime "reserve_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cab_id"], name: "index_reservations_on_cab_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,18 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_203115) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "cabs", force: :cascade do |t|
-    t.string "manufacturer"
-    t.text "description"
-    t.string "image_url"
-    t.string "transmission"
-    t.string "model"
-    t.float "rental_price"
-    t.integer "seating_capacity"
-    t.string "body_type"
-    t.integer "discount"
-    t.string "engine_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
+  add_foreign_key "reservations", "cabs"
+  add_foreign_key "reservations", "users"
 end
