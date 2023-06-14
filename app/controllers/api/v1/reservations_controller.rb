6c5bc470b971b29bc865fa44ebe1612137f5545a
Outkarm <jkpordje@gmail.com>
2023-06-14 10:03:25 +0000

    create and configure reservations controller

---
+++ app/controllers/api/v1/reservations_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::ReservationsController < ApplicationController
+  before_action :set_reservation, only: %i[show update destroy]
+
+  def index
+    @reservations = current_user.reservations.all
+    render json: @reservations
+  end
+
+  def show
+    render json: @reservation
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @reservation.update(reservation_params)
+      render json: @reservation
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reservation.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:reserve_date, :user_id, :cab_id, :user_name)
+  end
+end
