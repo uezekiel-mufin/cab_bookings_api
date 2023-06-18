f8db42eb5197bd5806958ae05ac87bcbd58c627e
Outkarm <jkpordje@gmail.com>
2023-06-18 02:28:48 +0000

    reconfigure devise, cors and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
-  get '/member-data', to: 'members#show'
-
   namespace :api do
     namespace :v1 do
       devise_for :users,
                  controllers: {
-                   sessions: 'users/sessions',
+                   sessions: 'api/v1/users/sessions',
                    registrations: 'api/v1/users/registrations'
                  }
       resources :cabs, only: [:index, :create, :show, :destroy]
       resources :reservations, only: [:index, :create, :destroy]
-
     end
   end
   # Defines the root path route ("/")
-  # root "articles#index"
-
   root "home#index"
 end
