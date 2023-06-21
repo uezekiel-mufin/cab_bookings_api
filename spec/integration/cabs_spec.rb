420115528e177fc37ada682fc0d92f291b0a57c9
Outkarm <jkpordje@gmail.com>
2023-06-21 11:33:10 +0000

    add an api documentation to project

--- spec/integration/cabs_spec.rb
+++ spec/integration/cabs_spec.rb
@@ -1,6 +1,54 @@
 require 'swagger_helper'
 
 describe 'Cabs API' do
+  path '/api/v1/users' do
+    post 'Creates a user' do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          user: {
+            type: :object,
+            properties: {
+              name: { type: :string },
+              email: { type: :string },
+              password: { type: :string },
+              password_confirmation: { type: :string }
+            },
+            required: %w[name email password password_confirmation]
+          }
+        }
+      }
+
+      response '201', 'user created' do
+        let(:user) do
+          {
+            user: {
+              name: 'john kpordje',
+              email: 'jkpordje@gmail.com',
+              password: 'your_password',
+              password_confirmation: 'your_password'
+            }
+          }
+        end
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:user) do
+          {
+            user: {
+              name: 'john kpordje',
+              email: 'jkpordje@gmail.com'
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
   path '/api/v1/cabs' do
     post 'Creates a cab' do
       tags 'Cabs'
@@ -20,12 +68,22 @@ describe 'Cabs API' do
           engine_type: { type: :string },
           user_id: { type: :integer }
         },
-        required: %w[manufacturer transmission image_url model rental_price seating_capacity
-                     engine_type user_id]
+        required: %w[manufacturer transmission image_url model rental_price seating_capacity engine_type user_id]
       }
 
       response '201', 'cab created' do
-        let(:cab) { { manufacturer: 'Ford', transmission: 'Automatic', image_url: 'http://example.com/car.jpg', model: 'Mustang', rental_price: 100, seating_capacity: 4, engine_type: 'Petrol', user_id: 1 } }
+        let(:cab) do
+          {
+            manufacturer: 'Ford',
+            transmission: 'Automatic',
+            image_url: 'http://example.com/car.jpg',
+            model: 'Mustang',
+            rental_price: 100,
+            seating_capacity: 4,
+            engine_type: 'Petrol',
+            user_id: 1
+          }
+        end
         run_test!
       end
 
@@ -58,10 +116,21 @@ describe 'Cabs API' do
                  engine_type: { type: :string },
                  user_id: { type: :integer }
                },
-               required: %w[id manufacturer transmission image_url model rental_price seating_capacity
-                            engine_type user_id]
+               required: %w[id manufacturer transmission image_url model rental_price seating_capacity engine_type
+                            user_id]
 
-        let(:id) { Cab.create(manufacturer: 'Ford', transmission: 'Automatic', image_url: 'http://example.com/car.jpg', model: 'Mustang', rental_price: 100, seating_capacity: 4, engine_type: 'Petrol', user_id: 1).id }
+        let(:id) do
+          Cab.create(
+            manufacturer: 'Ford',
+            transmission: 'Automatic',
+            image_url: 'http://example.com/car.jpg',
+            model: 'Mustang',
+            rental_price: 100,
+            seating_capacity: 4,
+            engine_type: 'Petrol',
+            user_id: 1
+          ).id
+        end
         run_test!
       end
 
@@ -79,16 +148,22 @@ describe 'Cabs API' do
       parameter name: :reservation, in: :body, schema: {
         type: :object,
         properties: {
+          city: { type: :string },
           reserve_date: { type: :string },
           cab_id: { type: :integer },
           user_id: { type: :integer }
         },
-        required: %w[reserve_date start_date end_date cab_id user_id]
+        required: %w[city reserve_date cab_id user_id]
       }
 
       response '201', 'reservation created' do
         let(:reservation) do
-          { reserve_date: '2023-06-18', cab_id: 1, user_id: 1 }
+          {
+            city: 'Accra',
+            reserve_date: '2023-06-18',
+            cab_id: 1,
+            user_id: 1
+          }
         end
         run_test!
       end
@@ -100,31 +175,33 @@ describe 'Cabs API' do
     end
   end
 
-  path '/api/v1/reservations/{id}' do
-    get 'Retrieves a reservation' do
+  path '/api/v1/reservations/' do
+    get 'Retrieves all reservations for a user' do
       tags 'Reservations'
       produces 'application/json'
-      parameter name: :id, in: :path, type: :integer
 
-      response '200', 'reservation found' do
-        schema type: :object,
-               properties: {
-                 id: { type: :integer },
-                 reserve_date: { type: :string },
-                 cab_id: { type: :integer },
-                 user_id: { type: :integer }
-               },
-               required: %w[id reserve_date start_date end_date cab_id user_id]
+      parameter name: :user, in: :query, type: :integer, required: true
 
-        let(:id) do
-          Reservation.create(reserve_date: '2023-06-18', cab_id: 1,
-                             user_id: 1).id
-        end
+      response '200', 'reservations found' do
+        schema type: :array,
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   city: { type: :string },
+                   reserve_date: { type: :string },
+                   cab_id: { type: :integer },
+                   user_id: { type: :integer }
+                 },
+                 required: %w[id city reserve_date cab_id user_id]
+               }
+
+        let(:user) { 1 }
         run_test!
       end
 
-      response '404', 'reservation not found' do
-        let(:id) { 999 }
+      response '404', 'user not found' do
+        let(:user) { 999 }
         run_test!
       end
     end
--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -22,7 +22,7 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'http://localhost:3000/api-docs',
+          url: 'http://localhost:3000',
           variables: {
             defaultHost: {
               default: 'www.example.com'
